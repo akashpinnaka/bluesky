c943e96a376b077311a185d0a42029955bd1734e
Akash Pinnaka <aakash.pinnaka@gmail.com>
2022-07-24 18:36:19 -0700

    Add Devise to the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
+  # devise_scope :user do  
+  #   get '/users/sign_out' => 'devise/sessions#destroy'     
+  # end
+
+  root to: "home#index"
+  resources :home, only: [] do
+    collection do
+      get "dashboard" => "home#dashboard"
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
